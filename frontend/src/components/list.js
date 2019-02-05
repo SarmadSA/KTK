db5423b3cc33d4e64ac94aa1ed308150b45e003f
nikitashum <n.sumahers@gmail.com>
2019-02-05 13:16:44 +0100

    Added list component

---
+++ frontend/src/components/list.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card'
+        import CardDeck from 'react-bootstrap/CardDeck'
+        import Button from 'react-bootstrap/Button'
+
+        const list = () => {
+    return (
+            <CardDeck>
+                <Card>
+                <Card.Img variant="top" src={require("../img/template.svg")} />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+                </Card>
+                <Card>
+                <Card.Img variant="top" src={require("../img/template.svg")} />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+                </Card>
+                <Card>
+                <Card.Img variant="top" src={require("../img/template.svg")} />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+                </Card>
+                <Card>
+                <Card.Img variant="top" src={require("../img/template.svg")} />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+                </Card>
+            </CardDeck>
+            );
+};
+export default list;
