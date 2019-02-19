2093f67f5ffd7b29bebeacc10623e5805f3f59ed
Sarmad <sarmadsa2@gmail.com>
2019-02-19 13:34:28 +0100

    Create address CRUD repository

---
+++ backend/src/main/java/com/activekids/web/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.activekids.web.repositories;
+
+import com.activekids.web.model.Address;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Long> {
+}
