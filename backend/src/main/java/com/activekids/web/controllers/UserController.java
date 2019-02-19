e92750ebc14cb73589ebcb25f7a2902899134344
Sarmad <sarmadsa2@gmail.com>
2019-02-19 13:36:59 +0100

    Create user rest controller

---
+++ backend/src/main/java/com/activekids/web/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.activekids.web.controllers;
+
+import com.activekids.web.model.User;
+import com.activekids.web.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin //TODO - Allow only authorized domains
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
