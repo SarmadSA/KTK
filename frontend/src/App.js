6fa06db8138014ce12016933356cead22ccb8d73
Nikita <n.sumahers@gmail.com>
2019-02-21 13:29:07 +0100

    Sign in updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,22 @@ import { Router } from 'react-router-dom';
 import History from './utils/History';
 import Routes from './components/Routes';
 import Footer from './components/Footer';
-import Nav from './components/navbar'
+import Nav from './components/navbar';
+import Signin from './components/SignIn';
 
-class App extends Component {
+class App extends Component {      
+    
+    displayNav(){
+        if (window.location.pathname.split("/").pop() !== "") {
+            return(<Nav/>);
+        }
+    }
+    
     render() {
         return (
                 <Router history={History}>
                     <div className="App">
-                        <Nav/>
+                        {this.displayNav()}       
                         <main className="main">
                             <Routes />
                             <div className="clear"/>
