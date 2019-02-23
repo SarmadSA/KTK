9065e22a5da0c84d70042e89b6a237f02cdc7bb4
Sarmad <sarmadsa2@gmail.com>
2019-02-23 15:41:45 +0100

    Refactor address to use lombok data

--- backend/src/main/java/com/activekids/web/model/Address.java
+++ backend/src/main/java/com/activekids/web/model/Address.java
@@ -1,7 +1,6 @@
 package com.activekids.web.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,31 +9,29 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Address {
 
     @Id
     @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter @Setter
     private Integer id;
 
     @Size(min = 2, max = 60)
     @NotNull
-    @Getter @Setter
     private String country;
 
     @Size(min = 2, max = 60)
     @NotNull
-    @Getter @Setter
     private String city;
 
     @Size(min = 2, max = 255)
     @NotNull
-    @Getter @Setter
     private String street;
 
     @NotNull
-    @Getter @Setter
     private Integer ZipCode;
 }
