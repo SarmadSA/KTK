3f43ea547a00f3e0c806c6524de0f83b394e46d5
Sarmad <sarmadsa2@gmail.com>
2019-02-26 11:47:23 +0100

    Fix nav bar bug & solve merge conflict

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { IoMdClose } from 'react-icons/io';
+import {IoMdClose} from 'react-icons/io';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import '../css/signInn.css';
 
 const SignIn = () => {
     return (
-            <div className='popup'>
-                <div className='popup_inner'>
-                    <h3>Log In</h3>
-                    <button className="closePop"><IoMdClose/></button>
-                    <Container>
-                        <Form className="SignForm">           
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Email" />
-                            </Form.Group>          
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                                <a className="signUp loginA" href="/url">Forgot password?</a>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicChecbox">
-                                <Form.Check type="checkbox" label="Remember me" />
-                            </Form.Group>
-                            <Button variant="light" type="submit">
-                                Log in
-                            </Button>
-                            <a className="signUp loginA" href="/url">Don't have an account?</a>
-                        </Form>
-                    </Container>
-                </div>
+        <div className='popup'>
+            <div className='popup_inner'>
+                <h3>Log In</h3>
+                <button className="closePop"><IoMdClose/></button>
+                <Container>
+                    <Form className="SignForm">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Email"/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"/>
+                            <a className="signUp loginA" href="/url">Forgot password?</a>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicChecbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+                        <Button variant="light" type="submit">Log in</Button>
+                        <a className="signUp loginA" href="/url">Don't have an account?</a>
+                    </Form>
+                </Container>
             </div>
-            );
+        </div>
+    );
 };
 export default SignIn;
--- frontend/src/pages/ListingPage.js
+++ frontend/src/pages/ListingPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Nav from '../components/navbar';
 import ListingP from '../components/ListingP';
 
-const ListingPage = () =>{
-    return (<div className="surround">
-                <Nav/>
-                <ListingP/>
-            </div>);
+const ListingPage = () => {
+    return (
+        <div className="surround">
+            <Nav/>
+            <ListingP/>
+        </div>
+    );
 };
 
 export default ListingPage;
