b306fcaabc31b073ee183c13fb1b933a3e74a611
Sarmad <sarmadsa2@gmail.com>
2019-03-01 19:49:35 +0100

    Use spring security to encrypt password before inserting to database

--- backend/src/main/java/com/activekids/web/services/UserServiceImp.java
+++ backend/src/main/java/com/activekids/web/services/UserServiceImp.java
@@ -4,16 +4,19 @@ import com.activekids.web.model.User;
 import com.activekids.web.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Service
 public class UserServiceImp implements UserService {
 
     private final UserRepository userRepository;
+    private BCryptPasswordEncoder encoder;
 
     @Autowired
     public UserServiceImp(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.encoder = new BCryptPasswordEncoder();
     }
 
     @Override
@@ -23,6 +26,9 @@ public class UserServiceImp implements UserService {
 
     @Override
     public boolean createUser(User user) {
+        //Hash password, hash -> salt + password. everything is done by this class.
+        String hashedPassword = this.encoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         userRepository.save(user);
         //TODO: check if user saved in database then return correct boolean value
         return true;
