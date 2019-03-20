90dd6f2d6e2a3785e983114843b4668cbca3dbe7
Sarmad <sarmadsa2@gmail.com>
2019-03-21 00:53:22 +0100

    Add JWT getter and remover

--- frontend/src/helpers/helperFunctions.js
+++ frontend/src/helpers/helperFunctions.js
@@ -4,3 +4,11 @@ import History from '../utils/History';
 export const goToPage = (path) =>{
     History.push(path);
 };
+
+export const getJWT = (jwtName) =>{
+    return localStorage.getItem(jwtName);
+};
+
+export const removeJWT = (jwtName) =>{
+    return localStorage.removeItem(jwtName);
+};
