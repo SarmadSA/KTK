0af8876288ac58bd4454568c3348fd53bcd37b75
Sarmad <sarmadsa2@gmail.com>
2019-03-21 01:00:43 +0100

    Fix many to many relation

--- backend/src/main/java/com/activekids/web/model/Role.java
+++ backend/src/main/java/com/activekids/web/model/Role.java
@@ -6,12 +6,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Role {
 
+    //@NotNull auto generated value, no need for not null (//TODO: only when you create database set to notnull)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -20,4 +22,6 @@ public class Role {
     @NotNull
     private String role;
 
+    @ManyToMany(mappedBy = "roles", cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<User> users;
 }
