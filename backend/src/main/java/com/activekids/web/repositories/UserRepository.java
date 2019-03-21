dc4b2060d98a372fa439302fe3a6433d8999d0c3
Sarmad <sarmadsa2@gmail.com>
2019-03-21 01:03:39 +0100

    Add getUserByEmail

--- backend/src/main/java/com/activekids/web/repositories/UserRepository.java
+++ backend/src/main/java/com/activekids/web/repositories/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     User getUserById(Integer id);
 
     Optional<User> findByEmail(String email);
+
+    User getUserByEmail(String email);
 }
