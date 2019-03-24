ceb95c8212e08eb5fa562a98d1e130317a4f99ae
Sarmad <sarmadsa2@gmail.com>
2019-03-24 02:30:42 +0100

    Edit consts file

--- frontend/src/resources/consts.js
+++ frontend/src/resources/consts.js
@@ -1,6 +1,6 @@
 export const CARD_PER_ROW = 4;
 export const CARD_PER_P_ROW = 3;
-export const AUTHENTICATION_JWT = "KTK-Auth-Token";
+export const AUTHENTICATION_JWT = "token";
 
 //Pages
 export const EXPLORE_PAGE = '/explore';
@@ -8,5 +8,6 @@ export const SIGNUP_PAGE = '/signup';
 export const PROFILE_PAGE = '/profile';
 
 //API
-export const AUTHENTICATION_API = 'http://localhost:8080/api/authentication';
-export const GET_LISTINGS_API = 'http://localhost:8080/listing/list';
+const HOST = "http://localhost:8080";
+export const AUTHENTICATION_API = HOST + '/api/authentication';
+export const GET_LISTINGS_API = HOST + '/listing/list';
