ff21b092c2b02b248726d40ac1d57e11aa530a74
Sarmad <sarmadsa2@gmail.com>
2019-03-24 02:38:26 +0100

    Fix card renderer and receive data from props

--- frontend/src/utils/CardRenderer.js
+++ frontend/src/utils/CardRenderer.js
@@ -5,11 +5,19 @@ import ListingCard from '../components/ListingCard';
 const CardRenderer = (props) => {
     const cardArray = [];
 
-    renderCards(props.number);
+    renderCards(props.number, props.data, props.from);
 
-    function renderCards(numOfCards) {
-        for(let i = 0; i < numOfCards; i++){
-            cardArray.push(<ListingCard/>);
+    function renderCards(numOfCards, data, from) {
+        for (let i = from; i < numOfCards + from; i++) {
+            cardArray.push(
+                <ListingCard
+                    title={data[i].title}
+                    description={data[i].description}
+                    image={data[i].image}
+                    age={data[i].age}
+                    country={data[i].country}
+                />
+            );
         }
     }
 
