cc94ff3a16e2477c4f788147ef5d29e342af2b82
Sarmad <sarmadsa2@gmail.com>
2019-03-24 02:39:36 +0100

    Update explore page to fetch data from server and show loading animation, correct errors and feedback

--- frontend/src/pages/ExplorePage.js
+++ frontend/src/pages/ExplorePage.js
@@ -2,41 +2,66 @@ import React, {Component} from 'react';
 import List from '../components/list';
 import Dropdown from '../components/Dropleft';
 import Text from '../components/text';
+import {executeHttpGet} from "../services/ApiClient";
+import {GET_LISTINGS_API} from "../resources/consts";
+import LoadingCube from "../components/LoadingCube";
 
 export default class LandingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [],
-            isLoading: true
+            isLoading: true,
+            requestNetworkError: false
         }
     }
 
     componentDidMount() {
-
         this.setState({isLoading: true});
-
-        //TODO check if data is fetched correctly before setting isLoading to false
-        fetch("URL HERE")
-            .then(response => response)
-            .then(data => this.setState({listings : data, isLoading : false}))
+        executeHttpGet(GET_LISTINGS_API, {}, this.onRequestSuccess, this.onRequestFailure);
     }
 
+    onRequestSuccess = (url, response) => {
+        if (response.data) {
+            this.setState({data: response.data, isLoading: false})
+        }
+    };
+
+    onRequestFailure = (url, response) => {
+        //Handle failure, display error or something to user
+        this.setState({isLoading: false});
+        if (!response.status) {
+            //Network error
+            this.setState({requestNetworkError: true})
+        } else {
+            //Other error get it from response
+        }
+    };
+
+    renderListings = () => {
+        const style = {marginTop:'10%'};
+
+        if (this.state.isLoading) {
+            return (<LoadingCube style={style}/>) //TODO - replace with loading component
+        }
+        if (this.state.data.length > 0) {
+            return (<List data={this.state.data}/>)
+        } else if (this.state.requestNetworkError) {
+            return (<div style={style}>Could not reach server, server may be offline. Please try again later!</div>)
+        } else {
+            return (<div style={style}>No listings available!</div>)
+        }
+    };
+
     renderContent = () => {
-        const {listings, isLoading} = this.state; //listings is array of data containing all listing objects
-        if (isLoading) {
-            return <p>Loading..</p> //TODO replace this with isLoading animation component
-        }
-        else if (!isLoading) {
-            return (
-                <div>
-                    <Text/>
-                    <hr/>
-                    <Dropdown/>
-                    <List/>
-                </div>
-            );
-        }
+        return (
+            <div>
+                <Text/>
+                <hr/>
+                <Dropdown/>
+                {this.renderListings()}
+            </div>
+        );
     };
 
     render() {
