fa74b6f4062b1e64a6d5aecfbe5b482860bd9d17
Sarmad <sarmadsa2@gmail.com>
2019-03-24 02:38:40 +0100

    Fix renderer

--- frontend/src/utils/ListngRowRenderer.js
+++ frontend/src/utils/ListngRowRenderer.js
@@ -8,33 +8,35 @@ import * as consts from '../resources/consts';
 const ListingRowRenderer = (props) => {
     const rowArray = [];
     const numberOfCardsToRender = props.number;
-    const numberOfRows = Math.ceil(numberOfCardsToRender/consts.CARD_PER_ROW);
+    const numberOfRows = Math.ceil(numberOfCardsToRender / consts.CARD_PER_ROW);
     const remainingCards = numberOfCardsToRender % consts.CARD_PER_ROW;
 
 
-    const getRow = (numOfCards) => {
+    const getRow = (numOfCards, from) => {
         return (
             <Row className="eachRow">
                 <CardDeck>
-                    <CardRenderer number={numOfCards}/>
+                    <CardRenderer data={props.data} number={numOfCards} from={from}/>
                 </CardDeck>
             </Row>
         );
     };
 
     const renderRows = (numOfRows) =>{
+
+        let lastRow = numOfRows; //3
+
+        if (remainingCards !== 0) {
+            lastRow = numOfRows - 1;
+        }
+        let renderFrom = 0;
         for(let rowNumber = 0; rowNumber < numOfRows; rowNumber++){
-            
-            let lastRow = numOfRows;
-            
-            if (remainingCards !== 0) {
-                lastRow = numOfRows - 1;
-            }
-            
             if(rowNumber === lastRow){
-                rowArray.push(getRow(remainingCards));
+                rowArray.push(getRow(remainingCards, renderFrom));
+                renderFrom += remainingCards;
             } else{
-                rowArray.push(getRow(consts.CARD_PER_ROW));
+                rowArray.push(getRow(consts.CARD_PER_ROW, renderFrom));
+                renderFrom += consts.CARD_PER_ROW;
             }
         }
     };
