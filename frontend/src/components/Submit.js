e76e28876705ce35feb665d8f0b7cb8d45bb599a
Sarmad <sarmadsa2@gmail.com>
2019-03-24 02:43:59 +0100

    Receive submitting data from props

--- frontend/src/components/Submit.js
+++ frontend/src/components/Submit.js
@@ -5,44 +5,46 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Countries from './countries';
 
-const Submit = () => {
+const Submit = (props) => {
     return (
-            <Container className='submitFormC'>
-                <Row>
-                    <Col md={{span: 6, offset: 3}}>
+        <Container className='submitFormC'>
+            <Row>
+                <Col md={{span: 6, offset: 3}}>
                     <div>
                         <Form>
                             <Form.Group controlId="formGridTitle">
                                 <Form.Label>Title</Form.Label>
-                                <Form.Control type="text" placeholder="Title" className="formContainer"/>
+                                <Form.Control type="text" placeholder="Title" className="formContainer" onChange={props.handleTitleChange}/>
                             </Form.Group>
                             <Form.Group controlId="formGridTextArea">
                                 <Form.Label>Description</Form.Label>
-                                <Form.Control as="textarea" rows="3" className="formContainer"/>
+                                <Form.Control as="textarea" rows="3" className="formContainer" onChange={props.handleDescriptionChange}/>
                             </Form.Group>
                             <Form.Row>
-                            <Form.Group as={Col} xs={6} md={6} controlId="formGridAge">
-                                <Form.Label>Age</Form.Label>
-                                <Form.Control type="number" placeholder="0"
-                                              className="formContainer"
-                                              defaultValue='0'/>
-                            </Form.Group>
-                            <Form.Group as={Col} xs={6} md={6} controlId="formGridCountry">
-                                <div className="label">
-                                    <Form.Label>Country</Form.Label>
-                                </div>
-                                <Countries/>
-                            </Form.Group>
+                                <Form.Group as={Col} xs={6} md={6} controlId="formGridAge">
+                                    <Form.Label>Age</Form.Label>
+                                    <Form.Control type="number" placeholder="0"
+                                                  className="formContainer"
+                                                  defaultValue='0'
+                                                  onChange={props.handleAgeChange}
+                                    />
+                                </Form.Group>
+                                <Form.Group as={Col} xs={6} md={6} controlId="formGridCountry">
+                                    <div className="label">
+                                        <Form.Label>Country</Form.Label>
+                                    </div>
+                                    <Countries handleChange={props.handleCountryChange}/>
+                                </Form.Group>
                             </Form.Row>
-                            <button type="submit" className="btn submitBtn">
+                            <button type="submit" className="btn submitBtn" onClick={props.handleFormSubmit}>
                                 Submit
                             </button>
                         </Form>
                     </div>
-                    </Col>
-                </Row>
-            </Container>
-                        );
-            };
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
-    export default Submit;
+export default Submit;
