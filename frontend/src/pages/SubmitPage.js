bf35aa5963f5158116265222b15a1338b696076d
Sarmad <sarmadsa2@gmail.com>
2019-03-26 18:08:43 +0100

    Handle image listing creation and image uploading and send/upload image to server

--- frontend/src/pages/SubmitPage.js
+++ frontend/src/pages/SubmitPage.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Uploader from '../components/Uploader';
 import Submit from '../components/Submit';
-import {executeHttpPut} from "../services/ApiClient";
+import {executeHttpPost, executeHttpPut} from "../services/ApiClient";
 import {getJWT} from "../helpers/helperFunctions";
-import {AUTHENTICATION_JWT} from "../resources/consts";
+import {AUTHENTICATION_JWT, CREATE_LISTING_API, UPLOAD_IMAGE_API} from "../resources/consts";
 
 const SubmitPage = () => {
-
-    const config = {
-        headers: {
-            'contentType': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' + getJWT(AUTHENTICATION_JWT)
-        }
-    };
 
     const formInput = {
         title: "",
@@ -20,6 +13,8 @@ const SubmitPage = () => {
         age: "",
         country: "",
     };
+
+    let imageFile = {};
 
     const handleTitleChange = (value) => {
         console.log("Title: " + value);
@@ -44,7 +39,33 @@ const SubmitPage = () => {
     const handleFormSubmit = (e) => {
         e.preventDefault();
         console.log("Submitting: " + formInput);
-        executeHttpPut("http://localhost:8080/listing/add", formInput, config, onSubmitSuccess, onSubmitFailure);
+
+        const formData = new FormData();
+        formData.append('file', imageFile);
+
+        executeHttpPost(
+            UPLOAD_IMAGE_API,
+            formData,
+            getConfig('multipart/form-data'),
+            onImageUploadSuccess,
+            onImageUploadFailure
+        );
+    };
+
+    const onImageUploadSuccess = () => {
+        console.log("Image upload success!");
+        console.log("Sending request to create listing..");
+        executeHttpPut(
+            CREATE_LISTING_API,
+            formInput,
+            getConfig('application/json; charset=UTF-8'),
+            onSubmitSuccess,
+            onSubmitFailure
+        );
+    };
+
+    const onImageUploadFailure = () => {
+        console.log("Upload failure!")
     };
 
     const onSubmitSuccess = () => {
@@ -52,12 +73,28 @@ const SubmitPage = () => {
     };
 
     const onSubmitFailure = () => {
-        console.log("Submit failure!")
+        console.log("Submit failure!");
+        //TODO - send request to delete uploaded image file
+    };
+
+    const handleImageChange = (images) =>{
+        console.log("Image info: ");
+        console.log(images[0]);
+        imageFile = images[0];
+    };
+
+    const getConfig = (contentType) =>{
+        return {
+            headers: {
+                'contentType': contentType,
+                'Authorization': 'Bearer ' + getJWT(AUTHENTICATION_JWT)
+            }
+        };
     };
 
     return (
         <div>
-            <Uploader/>
+            <Uploader handleImageChange={ (e) => handleImageChange(e) }/>
             <Submit
                 handleTitleChange={(e) => handleTitleChange(e.target.value)}
                 handleDescriptionChange={(e) => handleDescriptionChange(e.target.value)}
