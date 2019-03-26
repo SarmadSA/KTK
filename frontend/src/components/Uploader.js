8411d5150b1d14334434b7635309e95bd7e7210d
Sarmad <sarmadsa2@gmail.com>
2019-03-26 18:07:05 +0100

    Handle image upload value externally

--- frontend/src/components/Uploader.js
+++ frontend/src/components/Uploader.js
@@ -5,36 +5,45 @@ import '../css/upload.css';
 
 class Uploader extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             files: [],
             empty: true
         };
     }
 
+    formData = {
+        //name: this.state.files[0].name, // simply take the last file this.state.files[get last index].name,
+        //file: this.state.files[0]
+    };
+
     onDrop(files) {
-        this.setState({files});
-        this.setState({empty: false});
-        
-     //   const req = request.post('/upload')
-     //  files.forEach(file => {
-     //       req.attach(file.name, file)
-     //   })
-     //   req.end(callback)
+        this.setState({files: files, empty: false});
+
+        //this.formData.name = this.state.files[0].name;
+        //this.formData.file = this.state.files[0];
+        console.log("From state");
+        console.log(this.state.files);
+
+        //   const req = request.post('/upload')
+        //  files.forEach(file => {
+        //       req.attach(file.name, file)
+        //   })
+        //   req.end(callback)
+        files.forEach(file => {
+            console.log(file.name, file);
+        })
     }
 
     onCancel() {
-        this.setState({
-            files: []
-        });
-        this.setState({empty: true});
+        this.setState({files: [], empty: false});
     }
 
     render() {
         const files = this.state.files.map(file => (
-                    <p key={file.name}>{file.name}</p>
-                    ));
+            <p key={file.name}>{file.name}</p>
+        ));
 
         const baseStyle = {
             borderWidth: 2,
@@ -50,44 +59,46 @@ class Uploader extends React.Component {
         const rejectStyle = {
             borderStyle: 'solid',
             borderColor: '#c66',
-            backgroundColor : '#eee'
+            backgroundColor: '#eee'
         };
 
 
         return (
-                <section className="Submit">
-                    <h3>Submit your art.</h3>
-                    <br/>
-                    <Dropzone 
-                        accept="image/*" 
-                        onDrop={this.onDrop.bind(this)}
-                        onFileDialogCancel={this.onCancel.bind(this)}
-                        multiple="false"
-                        >
-                        {({
-                                getRootProps, getInputProps, isDragActive,
-                                        isDragAccept, isDragReject }) => {
-                                        let styles = {...baseStyle}
+            <section className="Submit">
+                <h3>Submit your art.</h3>
+                <br/>
+                <Dropzone
+                    accept="image/*"
+                    onDrop={ this.props.handleImageChange }
+                    onFileDialogCancel={this.onCancel.bind(this)}
+                    multiple="false"
+                >
+                    {({
+                          getRootProps, getInputProps, isDragActive,
+                          isDragAccept, isDragReject
+                      }) => {
+                        let styles = {...baseStyle};
                         styles = isDragActive ? {
-                                                    ...styles, ...activeStyle} : styles
+                            ...styles, ...activeStyle
+                        } : styles;
                         styles = isDragReject ? {
-                                                    ...styles, ...rejectStyle} : styles
-                
-                        return (
-                        <div className="fileZone" {
-                                                         ...getRootProps()} style={styles}>
-                            <input {...getInputProps()}/>
-                            <div>Click here or {isDragAccept ? 'drop' : 'drag'} the photo...</div>
-                            {isDragReject && <div>Unsupported file type...</div>}
-                        </div> )
-                        }}
-                    </Dropzone>
-                    <aside>
-                        <ul>{
-                                            files}</ul>
-                    </aside>
-                </section>);
-                        }
-            }
+                            ...styles, ...rejectStyle
+                        } : styles;
 
-            export default Uploader;
+                        return (
+                            <div className="fileZone" {
+                                ...getRootProps()} style={styles}>
+                                <input {...getInputProps()}/>
+                                <div>Click here or {isDragAccept ? 'drop' : 'drag'} the photo...</div>
+                                {isDragReject && <div>Unsupported file type...</div>}
+                            </div>)
+                    }}
+                </Dropzone>
+                <aside>
+                    <ul>{files}</ul>
+                </aside>
+            </section>);
+    }
+}
+
+export default Uploader;
