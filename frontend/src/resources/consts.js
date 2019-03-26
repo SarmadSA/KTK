371d3dad2f2b9e322ad410dd3dd549b813bcdbdb
Sarmad <sarmadsa2@gmail.com>
2019-03-26 18:05:07 +0100

    Update consts with new API's

--- frontend/src/resources/consts.js
+++ frontend/src/resources/consts.js
@@ -11,4 +11,6 @@ export const PROFILE_PAGE = '/profile';
 const HOST = "http://localhost:8080";
 export const AUTHENTICATION_API = HOST + '/api/authentication';
 export const GET_LISTINGS_API = HOST + '/listing/list';
-export const CREATE_USER_API = HOST + 'user/add"';
+export const UPLOAD_IMAGE_API = HOST + '/listing/uploadFile';
+export const CREATE_LISTING_API = HOST + '/listing/add';
+export const CREATE_USER_API = HOST + '/user/add';
