ee2b29db5c5c776c4d9b2b4129fb31b046bdbbe9
Sarmad <sarmadsa2@gmail.com>
2019-03-28 12:48:14 +0100

    Remove component

--- frontend/src/components/ListingCard.js
+++ frontend/src/components/ListingCard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Flag from 'react-world-flags';
