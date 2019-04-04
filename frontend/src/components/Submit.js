710a9b858af2ffa82d59cf1ff0f722586c58dac1
Sarmad <sarmadsa2@gmail.com>
2019-04-04 14:09:46 +0200

    Add error spans to all fields

--- frontend/src/components/Submit.js
+++ frontend/src/components/Submit.js
@@ -5,41 +5,49 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Countries from './countries';
 import TweetBox from './TweetBox';
+import '../css/submitForm.css';
 
 const Submit = (props) => {
     return (
-            <Container className='submitFormC'>
-                <Row>
-                    <Col md={{span: 6, offset: 3}}>
+        <Container className='submitFormC'>
+            <Row>
+                <Col md={{span: 6, offset: 3}}>
                     <div>
                         <Form>
                             <Form.Group controlId="formGridTitle">
                                 <Form.Label>Title</Form.Label>
-                                <Form.Control type="text" placeholder="Title" className="formContainer" onChange={props.handleTitleChange}/>
+                                <Form.Control type="text" placeholder="Title" className="formContainer"
+                                              onChange={props.handleTitleChange}/>
+                                <span className="error-message-span"> {props.handleTitleError} </span>
                             </Form.Group>
                             <Form.Group controlId="formGridName">
                                 <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="Name" className="formContainer" onChange={props.handleNameChange}/>
+                                <Form.Control type="text" placeholder="Name" className="formContainer"
+                                              onChange={props.handleNameChange}/>
+                                <span className="error-message-span"> {props.handleNameError} </span>
                             </Form.Group>
                             <Form.Group controlId="formGridTextArea">
                                 <Form.Label>Description</Form.Label>
                                 <TweetBox onChange={props.handleDescriptionChange}/>
+                                <span className="error-message-span"> {props.handleDescriptionError} </span>
                             </Form.Group>
                             <Form.Row>
                                 <Form.Group as={Col} xs={6} md={6} controlId="formGridAge">
                                     <Form.Label>Age</Form.Label>
                                     <Form.Control type="number" placeholder="0"
                                                   className="formContainer"
-                                                  defaultValue='0'
+                                                  defaultValue='1'
                                                   onChange={props.handleAgeChange}
-                                                  min="0"
-                                                  />
+                                                  min="1"
+                                    />
+                                    <span className="error-message-span"> {props.handleAgeError} </span>
                                 </Form.Group>
                                 <Form.Group as={Col} xs={6} md={6} controlId="formGridCountry">
                                     <div className="label">
                                         <Form.Label>Country</Form.Label>
                                     </div>
                                     <Countries handleChange={props.handleCountryChange}/>
+                                    <span className="error-message-span"> {props.handleCountryError} </span>
                                 </Form.Group>
                             </Form.Row>
                             <button type="submit" className="btn submitBtn" onClick={props.handleFormSubmit}>
@@ -47,10 +55,10 @@ const Submit = (props) => {
                             </button>
                         </Form>
                     </div>
-                    </Col>
-                </Row>
-            </Container>
-                        );
-            };
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
-    export default Submit;
+export default Submit;
